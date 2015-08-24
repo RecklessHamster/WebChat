d63193896ccb962c253efc8d7103a4f51d69d0f9
Nathan Ham <theninjahamster7@gmail.com>
2015-08-24 13:05:28 +0300

    Project Upload

---
+++ WebChat/ChatHub.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        
+        public static List<User> CurrentUsers = new List<User>();
+        //SqlConnection db = new SqlConnection();
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+            //updatedb();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {           
+            for(int i=0;i<CurrentUsers.Count;i++)
+            {
+                if(CurrentUsers[i].ConnectionId == this.Context.ConnectionId)
+                {
+                    CurrentUsers.RemoveAt(i);
+                }
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void RegisterUser(String name)
+        {
+            User newUser = new User(name, this.Context.ConnectionId);
+            CurrentUsers.Add(newUser);
+            Clients.All.updateUsers(JsonConvert.SerializeObject(CurrentUsers));          
+        }
+
+        public Int32 NumberofUsers()
+        {
+            return CurrentUsers.Count;
+        }
+        public List<User> SendUserList()
+        {
+            return CurrentUsers;
+        }
+    }
+}
---
+++ WebChat/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat
+{
+    public class User
+    {
+        public String Name { get; set; }
+        public String ConnectionId { get; set; }
+        public User(String name, String connectionId)
+        {
+            this.Name = name;
+            this.ConnectionId = connectionId;
+        }
+    }
+}
